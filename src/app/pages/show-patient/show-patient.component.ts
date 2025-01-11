3d2c317bc8ceda0b2867429dfd74d5e6046bf8b2
O009920 <mfuentes@forum.cl>
2025-01-11 16:52:34 -0300

    avance creacion componentes

---
+++ src/app/pages/show-patient/show-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-patient',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-patient.component.html',
+  styleUrl: './show-patient.component.css'
+})
+export class ShowPatientComponent {
+
+}
