ac083ad107eafb620fd48f9978c1fec56eece28e
O009920 <mfuentes@forum.cl>
2025-01-11 17:01:53 -0300

    componete listar paciente

--- src/app/pages/patient-list/patient-list.component.ts
+++ src/app/pages/patient-list/patient-list.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {Patient} from '../../core/models/patient';
+import {PatientService} from '../../core/services/patient.service';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-patient-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './patient-list.component.html',
   styleUrl: './patient-list.component.css'
 })
 export class PatientListComponent {
+
+  patients: Patient[] = [];
+  loading: boolean = true;
+
+  constructor(private patientService: PatientService) {}
+
+  ngOnInit(): void {
+    this.getPatients();
+  }
+
+  getPatients(): void {
+    this.patientService.getAllPatient().subscribe({
+      next: (data) => {
+        this.patients = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error al obtener pacientes:', error);
+        this.loading = false;
+      },
+    });
+  }
 
 }
