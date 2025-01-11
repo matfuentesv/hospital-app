3bfdb2dda24c80e28fdcd185b999eb2948cbeb2b
O009920 <mfuentes@forum.cl>
2025-01-11 16:55:36 -0300

    fix rutas paciente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {NgModule} from '@angular/core';
+import {CreatePatientComponent} from './pages/create-patient/create-patient.component';
+import {PatientListComponent} from './pages/patient-list/patient-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'patient', component: PatientListComponent},
   { path: '**', redirectTo: '' }
 ];
 
