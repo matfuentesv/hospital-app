ae54e381beb4ecd2caedcf21033b821c208d18a4
matfuentesv <mat.fuentes@duocuc.cl>
2025-02-15 22:40:41 -0300

    fix endpoint

--- src/app/enviroments/endpoints.ts
+++ src/app/enviroments/endpoints.ts
@@ -2,12 +2,12 @@ export const endpoints = {
   patients: {
     name: 'patient',
     description: 'Informacion de pacientes',
-    path: 'http://localhost:8085/api'
+    path: 'http://107.20.180.35:8085/api'
   },
   vitalSignsAlert:{
     name: 'vitalSigns',
     description: 'Informacion de signos vitales',
-    path: 'http://localhost:8080/api'
+    path: 'http://107.20.180.35:8086/api'
   },
   vitalSignsSummary:{
     name: 'vitalSigns',
